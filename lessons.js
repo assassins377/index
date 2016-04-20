3b7242bd9d58e3f5f44d78a634646d38fb5973db
assassins377 <wordctar@gmail.com>
2016-04-20 10:35:56 +0300

    lessons 1

---
+++ lessons.js
@@ -0,0 +1,30 @@
+// alert('Привет мир');
+// alert(3+1+2);
+// function addScript(src){
+// 	var script = document.createElement('script');
+// 	script.src = src;
+//   script.async = false; // чтобы гарантировать порядок
+//   document.head.appendChild(script);
+// }
+// addScript('js/modernizr.js'); // загружаться эти скрипты начнут сразу
+// addScript('js/jquery-1.9.1.min.js'); // выполнятся, как только загрузятся
+// addScript('js/okzoom.js'); // но, гарантированно, в порядке 1 -> 2 -> 3
+
+// Что особенно интересно – доллар '$' и знак подчеркивания '_' являются такими же обычными символами, как буквы:
+// var message;
+// var message="Привет я переменая";
+// alert(message);
+// var COLOR_RED = "#F00";
+// var COLOR_GREEN = "#0F0";
+// var COLOR_BLUE = "#00F";
+// var COLOR_ORANGE = "#FF7F00";
+
+// var color = COLOR_ORANGE;
+// alert( color ); // #FF7F00
+var checked=true; //true-истина
+checked=false; //false-ложь
+var ege=null;
+var get=undefined;
+var red=5;
+var ber=6;
+alert(red-ber);
